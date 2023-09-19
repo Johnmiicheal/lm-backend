eca730683caa6f4713bfaf31f284c32b7eef1c8e
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-09-19 22:24:19 +0100

    Page number and file name added

--- server.js
+++ server.js
@@ -12,14 +12,20 @@ const userRoutes = require("./routes/userRoutes.cjs");
 const pdf2img = require('pdf-img-convert');
 import http from "http";
 const fs = require('fs');
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-app.use(  cors({
-  credentials: true,
-  origin: [String(process.env.CORS_ORIGIN), "https://api.greynote.app/lecture"],
-}));
+app.use(
+  (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  }  
+//   cors({
+//   credentials: true,
+//   origin: "http://127.0.0.1:4000/api/upload/",
+// })
+);
 app.use( express.json() );
 app.use( express.urlencoded({ extended: true}) );
 
@@ -28,16 +34,16 @@ app.use("/api/auth", userRoutes);
 
 
 //Setup mongoose
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(async () => {
-  console.log("DB Connection Successful");
-})
-.catch((err) => {
-  console.log(err.message);
-});
+// mongoose.connect(process.env.MONGO_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(async () => {
+//   console.log("DB Connection Successful");
+// })
+// .catch((err) => {
+//   console.log(err.message);
+// });
 
 try{
       // Initialize Openai
@@ -86,13 +92,27 @@ try{
         }
       })
     })
+
+    const getPdf = (file) => {
+      if(!file){
+        res.status(500).json({
+          err: {
+          message: "Upload failed"
+        }
+      });
+        console.log("please upload your file");
+        return
+      }
+    }
     
     // PDF CUSTOMIZATION (EMBEDDING)
     app.post("/api/upload/", upload.single("file"), async (req, res) => { 
          
       try {
       console.log(req.file);
-      const userId = req.userId;
+      // const userId = req.userId;
+      // console.log(userId)
+    
       
       if(!req.file) {
         res.status(500).json({
@@ -128,7 +148,7 @@ try{
       
         try{
             // Setup UUIDV4
-            // const uniqueId = uuidv4();
+            const uniqueId = uuidv4();
     
             //Setup the image converter and OCR
             const outputImages2 = pdf2img.convert(req.file.path).catch(err => {res.status(502).json({err: {message: "Ensure your file is not corrupted and try again"}}); console.log(err);});
@@ -139,7 +159,7 @@ try{
                   return new Promise(resolve => setTimeout(resolve, ms));
                 }
                 for (var i = 0; i < (outputImages.length); i++){
-                  fs.writeFile("output" +userId+i+".png", outputImages[i], function (error) {
+                  fs.writeFile("output" +uniqueId+i+".png", outputImages[i], function (error) {
                     if (error) { console.error("Error: " + error); }
                   });
                     console.log("Page "+ i +" Done");
@@ -153,21 +173,22 @@ try{
                     for(var i = 0; i<outputImages.length; i++){                        
                           await worker.loadLanguage('eng');
                           await worker.initialize('eng');                
-                          var { data: { text } } = await worker.recognize('./output'+userId+i+".png");
+                          var { data: { text } } = await worker.recognize('./output'+uniqueId+i+".png");
+                          var textAndPageNumber = text + String(" $Page Number$: " + i + 1);
     
                           //Populate index
                             try {
                               const pdfEmbedding = await openai.createEmbedding({
                                 model: EMBEDDING_MODEL,
-                                input: text,
+                                input: textAndPageNumber,
                               });
-                              console.log(text);
+                              console.log(textAndPageNumber);
                     
                               const pdfTextEmbedding = pdfEmbedding.data.data[0].embedding;
                               console.log(pdfTextEmbedding);
                               
                               const pdfIndex = pinecone.Index("lecture-mate");
-                              const upsertRequest = { vectors: [ {id: "vec" + i , values: pdfTextEmbedding, metadata: { text: text }} ] , namespace: `lecture-mate-${userId}` };
+                              const upsertRequest = { vectors: [ {id: "vec" + i , values: pdfTextEmbedding, metadata: { text: textAndPageNumber }} ] , namespace: `lecture-mate-${uniqueId}` };
                     
                               //initalizing.....this will take up to 2 mins
                     
@@ -191,7 +212,7 @@ try{
                         }); 
                                         
                       }
-                        // res.json({uniqueId: uniqueId});                    
+                        res.json({uniqueId: uniqueId, fileName: req.file.filename});                    
                     }).catch(err => {console.log(err)});
                   }catch(err) {
                       res.status(500).json({err: {message: "There was an error processing your request"}});
@@ -210,4 +231,5 @@ let server = http.createServer(app);
 
 server.listen({ port: process.env.PORT }, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
-});
+}
+);
